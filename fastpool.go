0c2dfc27c1d455519e5463edf64a79cafe80aac1
Seis <xiaonanln@gmail.com>
2018-10-16 09:34:38 +0800

    use sync.Pool as fallback

--- fastpool.go
+++ fastpool.go
@@ -1,28 +1,33 @@
 package fastpool
 
-import "github.com/xiaonanln/go-lockfree-queue"
+import (
+	"github.com/xiaonanln/go-lockfree-queue"
+	"sync"
+)
 
 type FastPool struct {
-	q   *lfqueue.Queue
-	New func() interface{}
+	q        *lfqueue.Queue
+	fallback sync.Pool
 }
 
 func NewFastPool(capacity int, New func() interface{}) *FastPool {
 	fp := &FastPool{
-		q:   lfqueue.NewQueue(capacity),
-		New: New,
+		q: lfqueue.NewQueue(capacity),
 	}
+	fp.fallback.New = New
 	return fp
 }
 
 func (fp *FastPool) Put(x interface{}) {
-	fp.q.Put(x)
+	if ok := fp.q.Put(x); !ok {
+		fp.fallback.Put(x)
+	}
 }
 
 func (fp *FastPool) Get() interface{} {
 	if x, ok := fp.q.Get(); ok {
 		return x
 	} else {
-		return fp.New()
+		return fp.fallback.Get()
 	}
 }
